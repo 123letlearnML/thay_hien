7d47a82b5955d3d9601cf2d8bb17d0aca9dac9c3
123letlearnML <duongsanemi@gmail.com>
2025-05-12 13:17:50 +0700

    chinh sua lai loi sai 12/5

--- PTPMQL/mvc/obj/Debug/net9.0/mvc.AssemblyInfo.cs
+++ PTPMQL/mvc/obj/Debug/net9.0/mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a6e2c4b959778d83ed016e6bd45f8a26b8af09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6feb80fb521aba125241fdfa8cbb3a6048ae632")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
